b2226ce07dd5714b4246ccede37c7a493b96ebef
Thibaut Darolles <thibaut.darolles@gmail.com>
2020-02-01 19:45:38 +0100

    Ajoutez des fichiers projet.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace SubAdapter
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(12, 180);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(101, 31);
+            this.btn_browse.TabIndex = 0;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(15, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(258, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Tous les fichiers du dossier (saison)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(189, 21);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Seulement les fichiers FR";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(23, 59);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(132, 21);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Tous les fichiers";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(357, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 101);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Langues";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(299, 70);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Dossiers";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 60);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Lancer traitement";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(119, 184);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(788, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(913, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(36, 31);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 242);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 22);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(27, 278);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(635, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(210, 60);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Nouveau traitement";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Location = new System.Drawing.Point(173, 142);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 22);
+            this.numericUpDown1.TabIndex = 16;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Décalage en Secondes";
+            // 
+            // MainWindow
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(956, 312);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "SubAdapter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SubAdapter
+{
+
+    public partial class MainWindow : Form
+    {
+
+        //à faire en v2 : if tout le dossier sélectionné, faire un parallel for each sur tous les fr.srt du dossier
+        // à faire en v2 : if toutes les langues sélectionné, faire un parallel for each sur tous les srt du dossier avec le même nom d'épisode (donc parser le filename)
+        // à faire en v2 : if toutes les langues sélectionné et tout le dossier sélectionné , faire un parallel for each sur tous les srt du dossier
+        /// <summary>
+        /// exemple de sous titre  :
+        /// 
+//1
+//00:02:11,548 --> 00:02:12,966
+//Arrêtez de bouger.
+
+//2
+//00:02:13,967 --> 00:02:15,093
+//Arrêtez.
+
+//3
+//00:02:17,763 --> 00:02:18,931
+//C'est derrière.
+
+//4
+//00:02:24,019 --> 00:02:24,937
+//Tu m'expliques ?
+
+        /// 
+        /// </summary>
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        string filePath = string.Empty;
+        string fileName = string.Empty;
+        bool openFileOK = false;
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            ParserSrt();
+
+            textBox1.Text = filePath;
+        }
+
+        private bool ParserSrt()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "Z:\\downloads\\tv\\Big Little Lies\\Big.Little.Lies.S01E01.Somebodys.Dead.1080p.AMZN.WEBRip.DD5.1.x264-NTb";
+                openFileDialog.Filter = "srt files (*.srt)|*.srt";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return openFileOK = true;
+                    fileName = openFileDialog.FileName;
+//                    filePath = DirectoryInfo.Path; // voir cmt récupérer le chemin sélectionner
+
+                }
+                else
+                {
+                    return openFileOK = false;
+                }
+            }
+        }
+        private void ParserSrtAvecAnalyseLigne()
+        {
+            if (openFileOK == true)
+                {
+                    using (StreamReader reader = new StreamReader()) // voir cmt donner le filestream
+                    {
+                        try
+                        {
+                            string line = null;
+                            while ( (line = reader.ReadLine()) != null )
+                          
+                            { 
+                            // todo
+                            // si chaine vide je change de ss titre, à ce moment je vais chercher l'id suivant
+                            // si id alors chaine suivante = date début, puis "  -->   " date fin, puis texte jusqu'à chaine vide
+
+                            // datetime.parse pour décaler (ou parseexact) -- celui qui prend un formatteur en parametre
+                            // DateTime.Parse(string, provider) .AddSecond
+                            // créer un objet sstitre qui contient un id, une datedébut, datefin, texte
+
+                            //to do textbox nb de scondes de décalage
+
+
+                            }
+                            
+                           
+                        }
+                        catch (Exception ex)
+                        {
+                            if (ex.Message != null)
+                                textBox3.Visible = true;
+                            textBox3.Text = ex.Message;
+                        }
+                    }
+                }
+            else
+                {
+                MessageBox.Show("FichierMalImporté");
+                }
+            
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime ts = DateTime.Now;
+            btn_browse.Enabled = false;
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+            checkBox1.Enabled = false;
+            textBox1.Enabled = false;
+            button2.Enabled = false;
+
+
+            ParserSrtAvecAnalyseLigne();
+
+
+
+                textBox2.Visible = true;
+            textBox2.Text = string.Format("Temps mis : {0} ms", DateTime.Now.Subtract(ts).TotalMilliseconds);
+            button3.Visible = true;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox2.Visible = false;
+            button3.Visible = false;
+            btn_browse.Enabled = true;
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            checkBox1.Enabled = true;
+            textBox1.Enabled = true;
+            button2.Enabled = true;
+
+        }
+    }
+        
+}
